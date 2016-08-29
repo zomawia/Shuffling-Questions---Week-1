d844f33d8fdaae10920ab9bb724a6a1a1de1585a
zomawia <zomawia@gmail.com>
2016-08-28 23:32:56 -0700

    Circle vs Circle collision started

---
+++ Shuffling Questions - Week 1/Circle vs Circle Collision/Source.cpp
@@ -0,0 +1,49 @@
+//Zomawia Sailo
+//zomawia@gmail.com
+//
+//Problem: Circle vs.Circle Collision
+//	Circles are oftentimes used to simplify the space that an object may occupy.
+//	A valuable application of this is in determining whether or not two objects are overlapping.
+//
+//	Input :
+//	Ax : X position of circle A.
+//	Ay : Y position of circle A.
+//	Aradius : Radius of circle A.
+//	Bx : X position of circle B.
+//	By : Y position of circle B.
+//	Bradius : Radius of circle B.
+//
+//	Where these values are floating point values and the radius of each circle are positive.
+//
+//	Output :
+//	True or false, whether or not the circles are overlapping or touching.
+//
+//	Hint :
+//#include <cmath> for access to various common math tools such as the square root function (sqrtf).
+//
+//
+//	Example Input :
+//		 0.0 0.0 1.0 10.0 0.0 10.0
+//
+//			 Example Output :
+//		 true
+//
+//
+//			 Example Input :
+//		 0.0 0.0 1.0 3.0 3.0 2.0
+//
+//			 Example Output :
+//		 false
+//
+
+
+#include <iostream>
+#include <math.h>
+
+int main()
+
+{
+
+
+
+}
